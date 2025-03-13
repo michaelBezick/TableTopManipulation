5d992e24280be12cfb2dc907b6f090a7c1bb8c4c
michaelBezick <mbezick@purdue.edu>
2025-03-13 12:50:39 -0400

    Auto commit

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import numpy as np
-from PIL import Image
 import robosuite as suite
 from point_cloud_generator import PointCloudGenerator
+import matplotlib.pyplot as plt
 
 env = suite.make(
     env_name="Lift",  # try with other tasks like "Stack" and "Door"
@@ -9,7 +9,7 @@ env = suite.make(
     has_renderer=False,
     has_offscreen_renderer=True,
     use_camera_obs=True,
-    # camera_names=["", "custom_camera2"],
+    camera_names=['frontview', 'birdview', 'agentview', 'sideview', 'robot0_robotview', 'robot0_eye_in_hand'],
     camera_depths=True,
     camera_heights=128,
     camera_widths=128,
@@ -22,12 +22,17 @@ breakpoint()
 # reset the environment
 # breakpoint()
 
+camera_names = ['frontview', 'birdview', 'agentview', 'sideview', 'robot0_robotview', 'robot0_eye_in_hand']
+
 obs = env.reset()
 for i in range(1):
     action = np.random.randn(env.robots[0].dof)  # sample random action
     obs, reward, done, info = env.step(action)  # take action in the environment
 
-    breakpoint()
+    for camera in camera_names:
+        rgb_image = obs[camera + "_image"]
+        plt.imsave(camera + ".jpg", rgb_image)
+
 
 
 env.close()
