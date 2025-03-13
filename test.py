a12691fc5bd42eb9f67f774105e4400406a18190
michaelBezick <mbezick@purdue.edu>
2025-03-13 12:05:11 -0400

    Auto commit

--- test.py
+++ test.py
@@ -10,17 +10,26 @@ env = suite.make(
     has_offscreen_renderer=True,
     use_camera_obs=True,
     camera_names="agentview",
-    camera_heights=1024,
-    camera_widths=1024,
+    camera_depths=True,
+    camera_heights=128,
+    camera_widths=128,
 )
 
 # reset the environment
+# breakpoint()
 obs = env.reset()
 for i in range(1):
     action = np.random.randn(env.robots[0].dof)  # sample random action
     obs, reward, done, info = env.step(action)  # take action in the environment
-    camera_image = obs["agentview_image"]
-    image = Image.fromarray(camera_image)
-    image.save("test.png")
+
+    depth_image = obs["agentview_depth"]
+    # Normalize depth to 0-255 for saving
+    depth_image = (depth_image / depth_image.max() * 255).astype(np.uint8)
+
+    depth_image = np.squeeze(depth_image)
+
+    # Save depth image
+    depth_pil = Image.fromarray(depth_image)
+    depth_pil.save(f"depth_frame_{i}.png")
 
 env.close()
