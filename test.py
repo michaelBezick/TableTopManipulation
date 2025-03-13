cd99700822f14465cd351ef1e74811cdccb611ac
michaelBezick <mbezick@purdue.edu>
2025-03-13 12:45:26 -0400

    Auto commit

--- test.py
+++ test.py
@@ -2,13 +2,6 @@ import numpy as np
 from PIL import Image
 import robosuite as suite
 from point_cloud_generator import PointCloudGenerator
-from robosuite.models.arenas import Arena
-from robosuite.models.robots import RobotModel
-from robosuite.models.tasks import MujocoXML
-import os
-
-custom_xml_path = os.path.abspath("custom_xml_path")
-custom_mujoco_model = MujocoXML(custom_xml_path)
 
 env = suite.make(
     env_name="Lift",  # try with other tasks like "Stack" and "Door"
@@ -16,12 +9,11 @@ env = suite.make(
     has_renderer=False,
     has_offscreen_renderer=True,
     use_camera_obs=True,
-    camera_names=["custom_camera1", "custom_camera2"],
+    # camera_names=["", "custom_camera2"],
     camera_depths=True,
     camera_heights=128,
     camera_widths=128,
 )
-env.model = custom_mujoco_model
 
 breakpoint()
 
