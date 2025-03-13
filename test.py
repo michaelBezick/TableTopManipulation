be65787be5233fb14dbdac8586211468bac98a7d
michaelBezick <mbezick@purdue.edu>
2025-03-13 12:39:22 -0400

    Auto commit

--- test.py
+++ test.py
@@ -10,18 +10,11 @@ env = suite.make(
     has_renderer=False,
     has_offscreen_renderer=True,
     use_camera_obs=True,
-    camera_names=["custom_camera1", "custom_camera2"],
+    camera_names=['frontview', 'birdview', 'agentview', 'sideview', 'robot0_robotview', 'robot0_eye_in_hand'],
     camera_depths=True,
     camera_heights=128,
     camera_widths=128,
 )
-
-# Dynamically place cameras
-env.sim.model.cam_pos[env.sim.model.camera_name2id("custom_camera1")] = [0.5, 0.0, 1.0]
-env.sim.model.cam_pos[env.sim.model.camera_name2id("custom_camera2")] = [1.0, -0.5, 1.2]
-
-env.sim.model.cam_quat[env.sim.model.camera_name2id("custom_camera1")] = [1, 0, 0, 0]  # Quaternion for orientation
-env.sim.model.cam_quat[env.sim.model.camera_name2id("custom_camera2")] = [0.707, 0.707, 0, 0]  # 90-degree rotation
 
 breakpoint()
 
@@ -29,12 +22,16 @@ breakpoint()
 
 # reset the environment
 # breakpoint()
-obs = env.reset()
-for i in range(1):
-    action = np.random.randn(env.robots[0].dof)  # sample random action
-    obs, reward, done, info = env.step(action)  # take action in the environment
 
-    depth_image = obs["custom_camera1_image"]
-    # pcd = pcg.depthImageToPointCloud(depth_image)
+camera_names = ['frontview', 'birdview', 'agentview', 'sideview', 'robot0_robotview', 'robot0_eye_in_hand']
+
+for camera_name in camera_names:
+    obs = env.reset()
+    for i in range(1):
+        action = np.random.randn(env.robots[0].dof)  # sample random action
+        obs, reward, done, info = env.step(action)  # take action in the environment
+
+        breakpoint()
+
 
 env.close()
