a679af18f46aae385f1fd2dddf51486d17511184
michaelBezick <mbezick@purdue.edu>
2025-03-13 12:42:53 -0400

    Auto commit

--- test.py
+++ test.py
@@ -2,18 +2,21 @@ import numpy as np
 from PIL import Image
 import robosuite as suite
 from point_cloud_generator import PointCloudGenerator
+import os
 
-# create environment instance
+custom_xml_path = os.path.abspath("custom_xml_path")
+
 env = suite.make(
     env_name="Lift",  # try with other tasks like "Stack" and "Door"
     robots="Panda",  # try with other robots like "Sawyer" and "Jaco"
     has_renderer=False,
     has_offscreen_renderer=True,
     use_camera_obs=True,
-    camera_names=['frontview', 'birdview', 'agentview', 'sideview', 'robot0_robotview', 'robot0_eye_in_hand'],
+    camera_names=["custom_camera1", "custom_camera2"],
     camera_depths=True,
     camera_heights=128,
     camera_widths=128,
+    model=custom_xml_path,
 )
 
 breakpoint()
@@ -23,15 +26,12 @@ breakpoint()
 # reset the environment
 # breakpoint()
 
-camera_names = ['frontview', 'birdview', 'agentview', 'sideview', 'robot0_robotview', 'robot0_eye_in_hand']
+obs = env.reset()
+for i in range(1):
+    action = np.random.randn(env.robots[0].dof)  # sample random action
+    obs, reward, done, info = env.step(action)  # take action in the environment
 
-for camera_name in camera_names:
-    obs = env.reset()
-    for i in range(1):
-        action = np.random.randn(env.robots[0].dof)  # sample random action
-        obs, reward, done, info = env.step(action)  # take action in the environment
-
-        breakpoint()
+    breakpoint()
 
 
 env.close()
