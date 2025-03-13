b3a1451c136b2e85d4e7d14db931f8bc8964f0eb
michaelBezick <mbezick@purdue.edu>
2025-03-13 13:29:53 -0400

    Auto commit

--- euler.py
+++ euler.py
@@ -4,7 +4,15 @@ from scipy.spatial.transform import Rotation as R
 quat = [0.56, 0.43, 0.43, 0.56]
 euler_angles = R.from_quat(quat).as_euler('xyz', degrees=True)
 print(euler_angles)  # Output: [roll, pitch, yaw]
-euler_angles[1] -= 10  # Reduce pitch by 10 degrees
+euler_angles[1] -= 30  # Reduce pitch by 10 degrees
+# Convert modified Euler angles back to quaternion
+new_quat = R.from_euler('xyz', euler_angles, degrees=True).as_quat()
+print("Updated Quaternion:", new_quat)
+
+quat = [0.56, 0.43, 0.43, 0.56]
+euler_angles = R.from_quat(quat).as_euler('xyz', degrees=True)
+print(euler_angles)  # Output: [roll, pitch, yaw]
+euler_angles[1] += 30  # Reduce pitch by 10 degrees
 # Convert modified Euler angles back to quaternion
 new_quat = R.from_euler('xyz', euler_angles, degrees=True).as_quat()
 print("Updated Quaternion:", new_quat)
